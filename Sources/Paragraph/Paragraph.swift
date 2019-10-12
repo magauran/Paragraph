9edb4cb67e85e15ac91b7338e02353e509d0aed2
Alexey Salangin <a7ls@ya.ru>
2019-10-13 02:38:35 +0300

    Pass table and bundle for localization

--- Sources/Paragraph/Paragraph.swift
+++ Sources/Paragraph/Paragraph.swift
@@ -4,62 +4,102 @@ import Foundation
 prefix operator §
 
 public prefix func §(_ localization: AnyLocalization) -> String {
-    return NSLocalizedString(localization.key, comment: "")
-}
-
-public prefix func §(_ localization: CommentedLoclization) -> String {
-    return NSLocalizedString(localization.key, comment: localization.comment)
+    NSLocalizedString(
+        localization.key,
+        tableName: localization.table,
+        bundle: localization.bundle,
+        comment: localization.comment
+    )
 }
 
 public prefix func §(_ localization: DictLocalization) -> String {
-    let format = NSLocalizedString(localization.key, comment: localization.comment)
+    let format = NSLocalizedString(
+        localization.key,
+        tableName: localization.table,
+        bundle: localization.bundle,
+        comment: localization.comment
+    )
     return String(format: format, locale: .current, arguments: localization.arguments.map { $0.intValue })
 }
 
 public prefix func §(_ localization: FittingLocalization) -> String {
-    let string = NSLocalizedString(localization.key, comment: localization.comment) as NSString
+    let string = NSLocalizedString(
+        localization.key,
+        tableName: localization.table,
+        bundle: localization.bundle,
+        comment: localization.comment
+    ) as NSString
     return string.variantFittingPresentationWidth(localization.width)
 }
 
 // MARK: - Protocols
 public protocol AnyLocalization {
     var key: String { get }
-}
-
-public protocol CommentedLoclization: AnyLocalization {
+    var table: String? { get }
+    var bundle: Bundle { get }
     var comment: String { get }
 }
 
-public protocol DictLocalization: CommentedLoclization {
+public protocol DictLocalization: AnyLocalization {
     var arguments: [ArgumentType] { get }
 }
 
-public protocol FittingLocalization: CommentedLoclization {
+public protocol FittingLocalization: AnyLocalization {
     var width: Int { get }
 }
 
 // MARK: - Implementation
-public struct CommentedL10n: CommentedLoclization {
+public struct SimpleL10n: AnyLocalization {
     public let key: String
+    public let table: String?
+    public let bundle: Bundle
     public let comment: String
+
+    public init(key: String, table: String? = nil, bundle: Bundle = .main, comment: String) {
+        self.key = key
+        self.table = table
+        self.bundle = bundle
+        self.comment = comment
+    }
 }
 
 public struct DictL10n: DictLocalization {
     public let key: String
+    public let table: String?
+    public let bundle: Bundle
     public let comment: String
     public let arguments: [ArgumentType]
+
+    public init(key: String, arguments: [ArgumentType] = [], table: String? = nil, bundle: Bundle = .main, comment: String) {
+        self.key = key
+        self.table = table
+        self.bundle = bundle
+        self.comment = comment
+        self.arguments = arguments
+    }
 }
 
 public struct FittingL10n: FittingLocalization {
     public let key: String
+    public let table: String?
+    public let bundle: Bundle
     public let comment: String
     public let width: Int
+
+    public init(key: String, width: Int, table: String? = nil, bundle: Bundle = .main, comment: String) {
+        self.key = key
+        self.table = table
+        self.bundle = bundle
+        self.comment = comment
+        self.width = width
+    }
 }
 
 extension String: AnyLocalization {
-    public var key: String {
-        return self
-    }
+    public var key: String { self }
+    public var table: String? { nil }
+    public var bundle: Bundle { .main }
+    public var comment: String { "" }
 }
 
 public enum Gender: Int {
