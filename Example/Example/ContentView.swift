1ed2592f2f4cba43fb8c98ac11ae307f5ebf8e09
Alexey Salangin <a7ls@ya.ru>
2019-10-13 04:19:14 +0300

    Create iOS example

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Alexey Salangin on 10/12/19.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import SwiftUI
+import Paragraph
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text(§L10n.Main.helloText)
+            Text(§L10n.Account.days(5))
+            Text(§L10n.Account.birthday(.female))
+            Text(§L10n.Account.width(7))
+            Text(§L10n.Main.settingsButton)
+            Text(§L10n.Account.call(.male, 10))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/Example/Localization/L10n.swift
@@ -0,0 +1,25 @@
+//
+//  L10n.swift
+//  Example
+//
+//  Created by Alexey Salangin on 10/12/19.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import Paragraph
+
+enum L10n {
+    enum Main {
+        static let settingsButton = SimpleL10n(key: "Main.settingsButton", table: "Main", comment: "Title of the settings button")
+        static let helloText = "Main.helloText"
+    }
+
+    enum Account {
+        static let days: (Int) -> DictL10n = { DictL10n(key: "days", arguments: [$0], table: Self.table, comment: "") }
+        static let birthday: (Gender) -> DictL10n = { DictL10n(key: "birthday", arguments: [$0], table: Self.table, comment: "") }
+        static let width: (Int) -> FittingL10n = { FittingL10n(key: "width", width: $0, table: Self.table, comment: "") }
+        static let call: (Gender, Int) -> DictL10n = { DictL10n(key: "call", arguments: [$0, $1], table: Self.table, comment: "") }
+
+        private static let table = "Account"
+    }
+}
