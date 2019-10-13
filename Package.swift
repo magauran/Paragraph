e810e787d809870f6e0be0d7b07cf0b557a42c80
Alexey Salangin <a7ls@ya.ru>
2019-10-13 03:27:25 +0300

    Specify minimum iOS version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Paragraph",
+    platforms: [
+      .iOS(.v9)
+    ],
     products: [
         .library(
             name: "Paragraph",
